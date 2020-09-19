771d54ce82114a80c62d19999665106d4b10b24e
Richard <richardjalba@gmail.com>
2020-09-18 18:11:58 -0700

    added banner and nav bar

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
 import requests from "./requests";
+import "./Banner.css";
 
 function Banner() {
   const [movie, setMovie] = useState({});
@@ -20,14 +21,38 @@ function Banner() {
 
   console.log(movie);
 
+  /* Truncate copy and pasted from stack overflow */
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+  }
+
   return (
-    <header className="banner">
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundImage: `url(
+              'https://image.tmdb.org/t/p/original/${movie?.backdrop}'
+          )`,
+        backgroundPosition: "center center",
+      }}
+    >
       <div className="banner__contents">
-        {/* <<< background image */}
-        {/* Title */}
-        {/* div > 2 buttons */}
-        {/* description */}
+        <h1 className="banner__title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+
+        <div className="banner__buttons">
+          <button className="banner__button">PLAY</button>
+          <button className="banner__button">MY LIST</button>
+        </div>
+
+        <h1 className="banner__description">
+          {truncate(movie?.overview, 150)}
+        </h1>
       </div>
+
+      <div className="banner--fadeBottom" />
     </header>
   );
 }
